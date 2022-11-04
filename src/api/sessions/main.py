c289abcde5c8dc4f5ef28e467d92881e33f492ea
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-11-04 16:36:52 -0700

    cleanup create session logic

--- src/api/sessions/main.py
+++ src/api/sessions/main.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Body, Request, status
+from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
-from src.database.schema import Session
+from src.database.schema import Session, Trial
 from .models import *
 
 
@@ -14,11 +14,19 @@ router = APIRouter(prefix='/session')
     response_model=Session
 )
 def create_session(rq: Request, body: CreateSessionRq):
-    new_session = Session(
-        **jsonable_encoder(body)
-    )
+    # Check for duplicates
+    if rq.app.db['sessions'].find_one({'_id': body.id}) is not None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Session already exists: {body.id}"
+        )
+
+    # Create session and add to database
+    new_session = Session(**jsonable_encoder(body))
     new_document = jsonable_encoder(new_session)
     db_session = rq.app.db['sessions'].insert_one(new_document)
+
+    # Return document as response
     created_session = rq.app.db['sessions'].find_one(
         {'_id': db_session.inserted_id}
     )
@@ -28,7 +36,8 @@ def create_session(rq: Request, body: CreateSessionRq):
 @router.post(
     '/{session_id}/trial',
     status_code=status.HTTP_201_CREATED,
-    response_description='Created a new trial within existing session'
+    response_description='Created a new trial within existing session',
+    response_model=Trial
 )
 def create_trial_within_session(rq: Request, session_id: str):
     print(session_id)
