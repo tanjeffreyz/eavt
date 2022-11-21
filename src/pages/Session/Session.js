0017ae0e40fe8ddd1635cef9dddaca23e29f95c5
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-11-21 00:12:28 -0800

    loaded session data

--- src/pages/Session/Session.js
+++ src/pages/Session/Session.js
@@ -1,24 +1,37 @@
+import { useState, useEffect } from 'react';
 import { useParams, useOutletContext } from 'react-router-dom';
+import { sendRequest } from '../../utils';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import LoadingScreen from '../../components/LoadingScreen/LoadingScreen';
 
 function SessionNav() {
   const params = useParams();
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    sendRequest({
+      uri: `/sessions/${params.id}`,
+      pass: (data) => setSession(data)
+    })
+  }, []);
+
+  if (!session) return <LoadingScreen />;
   return NavigationBar({
     title: 'Session',
-    subtitle: params.id,
+    subtitle: session.path,
     links: [
       {name: 'TCA', to: {hash: 'tca'}},
       {name: 'Trials', to: {hash: 'trials'}}
     ],
-    context: ['asdfasdf']
+    context: [session]
   });
 }
 
 function Session() {
   const params = useParams();
-  const [test] = useOutletContext();
+  const [session] = useOutletContext();
   return (
-    <div>{test}</div>
+    <div>{session._id}</div>
   );
 }
 
