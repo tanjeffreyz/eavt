203f8ccd6029461b3e9d6a72c658801e6c5db2c0
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-11-21 17:56:02 -0800

    ocd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 const USE_STRICT = false;
 
