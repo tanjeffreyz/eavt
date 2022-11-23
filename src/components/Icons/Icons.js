49185af196c765c4ae63cb677e2c13b3a34f5c19
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-11-23 10:40:43 -0800

    added props to icons

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -1,21 +1,28 @@
 import home from './home.svg';
+import backChevron from './back-chevron.svg';
 import chevronLeft from './chevron-left.svg';
+import chevronRight from './chevron-right.svg';
 
 function getIcon(src) {
-  function Icon({ 
-    width='20px', 
-    height='20px' 
-  }) {
-    return <img src={src} width={width} height={height} alt={src} />;
+  function Icon(props) {
+    let { 
+      width=20, 
+      height=20
+    } = props;
+    return <img {...props} src={src} width={width} height={height} alt={src} />;
   }
   return Icon;
 }
 
 
 const Home = getIcon(home);
-const Back = getIcon(chevronLeft);
+const Back = getIcon(backChevron);
+const ChevronLeft = getIcon(chevronLeft);
+const ChevronRight = getIcon(chevronRight);
 
 export {
   Home,
-  Back
+  Back,
+  ChevronLeft,
+  ChevronRight
 };
