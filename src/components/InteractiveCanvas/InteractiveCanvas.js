925a6c34696730803cc6caa4b0cfdce1b2269a2f
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-11-22 19:16:35 -0800

    three.js panning somewhat works, after creating mesh, no need to create again!

--- src/components/InteractiveCanvas/InteractiveCanvas.js
+++ src/components/InteractiveCanvas/InteractiveCanvas.js
@@ -1,5 +1,10 @@
 import './InteractiveCanvas.css';
 import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+const FOV = 45;
+const NEAR = 1;
+const FAR = 100;
 
 function InteractiveCanvas({
   width,
@@ -7,48 +12,111 @@ function InteractiveCanvas({
   drawFunction=defaultDraw
 }) {
   const canvasRef = useRef();
-  const contextRef = useRef(null);
+  const rendererRef = useRef(null);
+  const sceneRef = useRef(null);
+  const cameraRef = useRef(null);
+
   const mouseStartRef = useRef({ x: 0, y: 0 });
+  const cameraStartRef = useRef({ x: 0, y: 0 });
   const draggingRef = useRef(false);
-  const imageSizeRef = useRef({width: 1, height: 1});
+  const imageSizeRef = useRef({ width: 1, height: 1 });
+
+  // Setup Three.js renderer
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.addEventListener('mousedown', onMouseDown);
+    canvas.addEventListener('mousemove', onMouseMove);
+    canvas.addEventListener('mouseup', onMouseUp);
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true
+    });
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      FOV, width / height, NEAR, FAR
+    );
+    camera.position.set(0, 0, FAR);
+    scene.add(camera);
+
+    rendererRef.current = renderer;
+    sceneRef.current = scene;
+    cameraRef.current = camera;
+
+    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    const points = [];
+    points.push( new THREE.Vector3( - 10, 0, 0 ) );
+    points.push( new THREE.Vector3( 0, 10, 0 ) );
+    points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    scene.add(line);
+
+    // draw();
+    console.log('mounted');
+  }, []);
 
   // Prepare draw function
-  function draw() {   // TODO: Use callback!!!!
-    const context = contextRef.current;
-    const tl = context.transformPoint({
-      x: 0,
-      y: 0
+  function draw() {
+    requestAnimationFrame((time) => {
+      // drawFunction(sceneRef.current);
+      rendererRef.current.render(sceneRef.current, cameraRef.current);
     });
-    const br = context.transformPoint({
-      x: canvasRef.current.width,
-      y: canvasRef.current.height
-    });
-    context.clearRect(tl.x, tl.y, br.x - tl.x, br.y - tl.y);
-    return drawFunction(context);
   }
+  // function draw() {   // TODO: Use callback!!!!
+  //   const context = contextRef.current;
+  //   const tl = context.transformPoint({
+  //     x: 0,
+  //     y: 0
+  //   });
+  //   const br = context.transformPoint({
+  //     x: canvasRef.current.width,
+  //     y: canvasRef.current.height
+  //   });
+  //   context.clearRect(tl.x, tl.y, br.x - tl.x, br.y - tl.y);
+  //   return drawFunction(context);
+  // }
 
-  // Event listeners
-  // Listeners can only access initial state, so use refs instead
+  // // Event listeners
+  // // Listeners can only access initial state, so use refs instead
   function onMouseDown(e) {
-    mouseStartRef.current = contextRef.current.transformPoint({
+    const camera = cameraRef.current;
+    mouseStartRef.current = {
       x: e.offsetX,
       y: e.offsetY
-    });
+    };
+    cameraStartRef.current = {
+      x: camera.position.x,
+      y: camera.position.y
+    }
+    console.log(mouseStartRef.current);
     draggingRef.current = true;
   }
 
   function onMouseMove(e) {
     if (draggingRef.current) {
-      const context = contextRef.current;
-      const curr = context.transformPoint({
+      const camera = cameraRef.current;
+      const mouseStart = mouseStartRef.current;
+      const cameraStart = cameraStartRef.current;
+      // const context = contextRef.current;
+      const mouseCurr = {
         x: e.offsetX,
         y: e.offsetY
-      });
-      const start = mouseStartRef.current;
+      };
+      
+      const deltaX = mouseStart.x - e.offsetX;
+      const deltaY = e.offsetY - mouseStart.y;
+      camera.position.set(
+        cameraStart.x + deltaX,
+        cameraStart.y + deltaY
+      );
+
+      console.log(mouseCurr.x - mouseStart.x, mouseCurr.y - mouseStart.y);
       // console.log(curr.x, curr.y, x, y);
       // console.log(currX - x, currY - y);
       // console.log(contextRef.current.translate);
-      context.translate(curr.x - start.x, curr.y - start.y);
+      // context.translate(curr.x - start.x, curr.y - start.y);
       
       draw();
       // render({});
@@ -58,24 +126,12 @@ function InteractiveCanvas({
   function onMouseUp(e) {
     draggingRef.current = false;
   }
-
-  // Initialize canvas
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mouseup', onMouseUp);
-    contextRef.current = canvas.getContext('2d');
-    bindCanvasContext(contextRef.current);
-    draw(contextRef.current);
-    console.log('mounted');
-  }, []);
   
-  // Draw frame on render
-  requestAnimationFrame((time) => {
-    imageSizeRef.current = draw(contextRef.current);
-    // console.log(imageSizeRef.current);
-  });
+  // // Draw frame on render
+  // requestAnimationFrame((time) => {
+  //   imageSizeRef.current = draw(contextRef.current);
+  //   // console.log(imageSizeRef.current);
+  // });
   console.log('rendered');
   
   // Render component
@@ -94,30 +150,42 @@ function InteractiveCanvas({
   );
 }
 
-function bindCanvasContext(context) {
-  // Transform point from document space to canvas space
-  context.transformPoint = function({x, y}) {
-      return this.getTransform().inverse().transformPoint({x, y});
-  }
+// function bindCanvasContext(context) {
+//   // Transform point from document space to canvas space
+//   context.transformPoint = function({x, y}) {
+//       return this.getTransform().inverse().transformPoint({x, y});
+//   }
 
-  // Track canvas scaling using the determinant
-  const t = context.getTransform();
-  const initialScale = Math.sqrt(t.a * t.d - t.b * t.c);
-  context.getScaleFactor = function() {
-      const m = this.getTransform();
-      return Math.sqrt(m.a * m.d - m.b * m.c) / initialScale;
-  }
+//   // Track canvas scaling using the determinant
+//   const t = context.getTransform();
+//   const initialScale = Math.sqrt(t.a * t.d - t.b * t.c);
+//   context.getScaleFactor = function() {
+//       const m = this.getTransform();
+//       return Math.sqrt(m.a * m.d - m.b * m.c) / initialScale;
+//   }
+// }
+
+function defaultDraw(scene) {
+  const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+  const points = [];
+  points.push( new THREE.Vector3( - 10, 0, 0 ) );
+  points.push( new THREE.Vector3( 0, 10, 0 ) );
+  points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+  const line = new THREE.Line(geometry, material);
+  scene.add(line);
 }
 
-function defaultDraw(context) {
-  context.fillStyle = 'red';
-  context.fillRect(0, 0, 400, 400);
+// function defaultDraw(context) {
+//   context.fillStyle = 'red';
+//   context.fillRect(0, 0, 400, 400);
 
-  // Draw functions should return the size of largest image it drew
-  return {
-    width: 1, 
-    height: 1
-  };
-}
+//   // Draw functions should return the size of largest image it drew
+//   return {
+//     width: 1, 
+//     height: 1
+//   };
+// }
 
 export default InteractiveCanvas;
