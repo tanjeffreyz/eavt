dfd9081b3e894129d75660647a3871a23bd50fc3
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-11-23 20:44:00 -0800

    add comments to sessions

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,6 @@
 from src.common import config
 from .sessions.routes import router as sessions_router
+from .session_data.routes import router as session_data_router
 from .trials_within_session.routes import router as trials_within_session_router
 from .trials.routes import router as trials_router
 from .trial_data.routes import router as trial_data_router
@@ -7,6 +8,7 @@ from .trial_data.routes import router as trial_data_router
 
 ROUTERS = (
     sessions_router,
+    session_data_router,
     trials_within_session_router,
     trials_router,
     trial_data_router
---
+++ src/api/session_data/routes.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Request, status
+from fastapi.encoders import jsonable_encoder
+from src.api.utils import get_document_by_id
+from src.database.schema import Session, Comment
+
+
+router = APIRouter(
+    prefix='/sessions',
+    tags=['Session Data']
+)
+
+
+#########################
+#       Comments        #
+#########################
+@router.post(
+    '/{session_id}/comments',
+    status_code=status.HTTP_201_CREATED,
+    description='Adds a new comment to this session',
+    response_model=Session
+)
+async def add_comment_to_session(rq: Request, session_id: str, body: Comment):
+    session = Session(**get_document_by_id(rq.app.db['sessions'], session_id))
+    rq.app.db['sessions'].update_one(
+        {'_id': session.id},
+        {'$push': {'comments': jsonable_encoder(body)}}
+    )
+    return get_document_by_id(rq.app.db['sessions'], session_id)
+
+
+@router.delete(
+    '/{session_id}/comments/{comment_id}',
+    description='Deletes the comment from this session',
+    response_model=Session
+)
+async def delete_comment_from_session(rq: Request, session_id: str, comment_id: str):
+    session = Session(**get_document_by_id(rq.app.db['sessions'], session_id))
+    rq.app.db['sessions'].update_one(
+        {'_id': session.id},
+        {'$pull': {'comments': {'_id': comment_id}}}
+    )
+    return get_document_by_id(rq.app.db['sessions'], session_id)
